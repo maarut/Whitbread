e25c829165752d6bcd9da2ebee2dba6cb936105d
Maarut Chandegra <contact@maarut.net>
2019-01-22 08:27:04 +0000

    Add FourSquare API details and appropriate config file for the same

---
+++ Whitbread/Environments/MCConstants.h
@@ -0,0 +1,16 @@
+//
+//  MCConstants.h
+//  Whitbread
+//
+//  Created by Maarut Chandegra on 22/01/2019.
+//  Copyright © 2019 Maarut Chandegra. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+extern NSString *const kFourSquareClientId;
+extern NSString *const kFourSquareClientSecret;
+
+@interface MCConstants : NSObject
+
+@end
---
+++ Whitbread/Environments/MCConstants.m
@@ -0,0 +1,16 @@
+//
+//  MCConstants.m
+//  Whitbread
+//
+//  Created by Maarut Chandegra on 22/01/2019.
+//  Copyright © 2019 Maarut Chandegra. All rights reserved.
+//
+
+#import "MCConstants.h"
+
+NSString *const kFourSquareClientSecret = FOURSQUARE_CLIENT_SECRET;
+NSString *const kFourSquareClientId = FOURSQUARE_CLIENT_ID;
+
+@implementation MCConstants
+
+@end
