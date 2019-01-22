44bb947455e6334f799f7570f2d9e3773e149f27
Maarut Chandegra <contact@maarut.net>
2019-01-22 08:07:02 +0000

    Initial Commit

---
+++ Whitbread/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Whitbread
+//
+//  Created by Maarut Chandegra on 22/01/2019.
+//  Copyright © 2019 Maarut Chandegra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
